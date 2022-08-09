4791de380f326c385b64bfe7846e6f1cdcbaa002
Cesmac <cesmac@academico.cesmac.edu.br>
2022-08-09 20:26:55 -0300

    exercicio 2

--- 01_ruby_e_testes/test_minitest.rb
+++ 01_ruby_e_testes/test_minitest.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 
 class TestMinitest < Minitest::Test
         def test_sucesso
-            nome = 'Rachid'
-            assert_equal 'Rachid' , nome
+            nome = 'Lucas'
+            assert_equal 'Lucas' , nome
         end
 end
