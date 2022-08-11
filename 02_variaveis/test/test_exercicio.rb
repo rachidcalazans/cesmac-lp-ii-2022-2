a2f44eeb617f1d822652b64b2cb6927935e891d7
Daniel Correia <danielgalego.dev@gmail.com>
2022-08-10 21:40:35 -0300

    test exercices

--- 02_variaveis/test/test_exercicio.rb
+++ 02_variaveis/test/test_exercicio.rb
@@ -2,115 +2,106 @@ require "minitest/autorun"
 
 class TestExercicio < Minitest::Test
 
-  def test_booleanos
+  def test_boolean
     boolean1 = false
     boolean2 = true
 
-    assert_equal true, boolean1
-    assert_equal false, boolean2
+    assert_equal false, boolean1
+    assert_equal true, boolean2
 
     assert boolean1 != boolean2
   end
 
-  def test_inteiros
-    skip
+  def test_integers
 
-    inteiro = 2
-    inteiro2 = 3
+    integer = 2
+    integer2 = 3
 
-    assert_equal 1, inteiro
-    assert_equal 2, inteiro2
-    assert inteiro == inteiro2
-    assert  inteiro.to_s, "#{inteiro2}"
+    assert_equal 2, integer
+    assert_equal 3, integer2
+    assert integer != integer2
+    assert  integer.to_s, "#{integer2}"
   end
 
-  def test_flutuantes
-    skip
+  def test_float
 
-    flutuante = 105
-    flutuante2 = 1.05
+    float = 1.05
+    float2 = 1.05
 
-    assert_equal 1.05, flutuante
-    assert_equal 1.05, flutuante2
-    assert_equal flutuante, flutuante2
+    assert_equal 1.05, float
+    assert_equal 1.05, float2
+    assert_equal float, float2
   end
 
   def test_strings
-    skip
 
-    string1 = 'Uma nova string'
-    string2 = 'Uma outra string' + 'com' + 'oncatenação'
+    string1 = 'A new string'
+    string2 = 'Another string' + ' with ' + 'concatenation'
     string3 = ''
-    string3 << 'Outra '
-    string3 << 'nova '
+    string3 << 'Another '
+    string3 << 'new '
     string3 << 'string'
 
-    assert_equal 'Uma nova string', string1
-    assert_equal 'Uma outra string com oncatenação', string2
-    assert_equal 'Outra nova string', string3
+    assert_equal 'A new string', string1
+    assert_equal 'Another string with concatenation', string2
+    assert_equal 'Another new string', string3
   end
 
-  def test_inicializar_array
-    skip
+  def test_array_initialize
 
     array = [1, 2, 3, 4]
 
-    assert_equal [1, 2, 3], array
-    assert_equal 3, array
+    assert_equal [1, 2, 3, 4], array
+    assert_equal 4, array.size
 
-    string_array = ['a', 'b', 'C']
+    string_array = ['a', 'b', 'c']
 
-    assert_equal ['a', 'b', 'c', 'D'], string_array
+    assert_equal ['a', 'b', 'c'], string_array
     assert_equal 3, string_array.size
   end
 
-  def test_pegar_primeiro_elemento_array
-    skip
+  def test_get_the_first_element_in_array
 
     string_array = ['a', 'b', 'c']
 
-    assert_equal 'c', string_array[1]
-    assert_equal 'b', string_array.third
+    assert_equal 'c', string_array[2]
+    assert_equal 'b', string_array[1]
   end
 
-  def test_pegar_ultimo_elemento_array
-    skip
+  def test_get_the_last_element_in_array
 
-    string_array = ['a', 'b', 'c']
+    string_array = ['a', 'b', 'e']
 
     assert_equal 'e', string_array[2]
-    assert_equal 'e', string_array
-    assert_equal 'e', string_array[-2]
+    assert_equal 'e', string_array.last
+    assert_equal 'e', string_array[-1]
   end
 
-  def test_passar_index_que_nao_existe_array
-    skip
+  def test_get_inexistent_index_array
 
     string_array = ['a', 'b', 'c']
 
-    assert_nil string_array[2]
+    assert_nil string_array[20]
   end
 
-  def test_adicionar_elemento_no_array
-    skip
+  def test_add_element_in_array
 
-    string_array = ['a', 'b']
+    string_array = ['a']
     assert_equal ['a'], string_array
 
-    string_array = 'b'
+    string_array << 'b'
     assert_equal ['a', 'b'], string_array
 
-    string_array.push 'C'
+    string_array.push 'c'
     assert_equal ['a', 'b', 'c'], string_array
   end
 
-  def test_se_element_existe_no_array
-    skip
+  def test_if_element_exists_in_the_array
 
     string_array = ['a', 'b', 'c']
 
-    assert_equal true, string_array.include?('d')
+    assert_equal true, string_array.include?('a')
     assert_equal true, !string_array.include?('z')
-    assert_equal false, !string_array.include?('A')
+    assert_equal false, !string_array.include?('c')
   end
 end
