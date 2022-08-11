e828f9cac58ae8d27a0db547a395c664f28d7526
Silvio Romero <silvioromeroneto@gmail.com>
2022-08-10 21:44:07 -0300

    exercice 03 accomplished

--- 02_variaveis/test/test_exercicio.rb
+++ 02_variaveis/test/test_exercicio.rb
@@ -2,12 +2,14 @@ require "minitest/autorun"
 
 class TestExercicio < Minitest::Test
 
-  def test_booleanos
+  def test_booleans
+    skip
+
     boolean1 = false
     boolean2 = true
 
-    assert_equal true, boolean1
-    assert_equal false, boolean2
+    assert_equal false, boolean1
+    assert_equal true, boolean2
 
     assert boolean1 != boolean2
   end
@@ -15,102 +17,102 @@ class TestExercicio < Minitest::Test
   def test_inteiros
     skip
 
-    inteiro = 2
-    inteiro2 = 3
+    integer = 2
+    integer2 = 3
 
-    assert_equal 1, inteiro
-    assert_equal 2, inteiro2
-    assert inteiro == inteiro2
-    assert  inteiro.to_s, "#{inteiro2}"
+    assert_equal 2, integer
+    assert_equal 3, integer2
+    assert integer != integer2
+    assert  integer.to_s, "#{integer2}"
   end
 
-  def test_flutuantes
+  def test_floating
     skip
 
-    flutuante = 105
-    flutuante2 = 1.05
+    floating = 1.05
+    floating2 = 1.05
 
-    assert_equal 1.05, flutuante
-    assert_equal 1.05, flutuante2
+    assert_equal 1.05, floating
+    assert_equal 1.05, floating2
     assert_equal flutuante, flutuante2
   end
 
   def test_strings
     skip
 
-    string1 = 'Uma nova string'
-    string2 = 'Uma outra string' + 'com' + 'oncatenação'
+    string1 = 'A new string'
+    string2 = 'A new string' + ' with' + ' concatenation'
     string3 = ''
-    string3 << 'Outra '
-    string3 << 'nova '
+    string3 << 'Another '
+    string3 << 'new '
     string3 << 'string'
 
-    assert_equal 'Uma nova string', string1
-    assert_equal 'Uma outra string com oncatenação', string2
-    assert_equal 'Outra nova string', string3
+    assert_equal 'A new string', string1
+    assert_equal 'A new string with concatenation', string2
+    assert_equal 'Another new String', string3
   end
 
-  def test_inicializar_array
+  def test_initialize_array
     skip
 
     array = [1, 2, 3, 4]
 
-    assert_equal [1, 2, 3], array
-    assert_equal 3, array
+    assert_equal [1, 2, 3, 4], array
+    assert_equal 4, array
 
     string_array = ['a', 'b', 'C']
 
-    assert_equal ['a', 'b', 'c', 'D'], string_array
+    assert_equal ['a', 'b', 'C'], string_array
     assert_equal 3, string_array.size
   end
 
-  def test_pegar_primeiro_elemento_array
+  def test_get_the_first_element_in_array
     skip
 
     string_array = ['a', 'b', 'c']
 
-    assert_equal 'c', string_array[1]
-    assert_equal 'b', string_array.third
+    assert_equal 'a', string_array[0]
+    assert_equal 'a', string_array.first
   end
 
-  def test_pegar_ultimo_elemento_array
+  def test_get_the_last_element_in_array
+  skip
+
+    string_array = ['a', 'b', 'c']
+
+    assert_equal 'c', string_array[2]
+    assert_equal 'c', string_array.last
+    assert_equal 'c', string_array[-2]
+  end
+
+  def test_pass_index_that_dosent_exist_in_array
     skip
 
     string_array = ['a', 'b', 'c']
 
-    assert_equal 'e', string_array[2]
-    assert_equal 'e', string_array
-    assert_equal 'e', string_array[-2]
+    assert_nil string_array[5]
   end
 
-  def test_passar_index_que_nao_existe_array
+  def test_add_the_element_in_array
     skip
 
-    string_array = ['a', 'b', 'c']
-
-    assert_nil string_array[2]
-  end
-
-  def test_adicionar_elemento_no_array
-    skip
-
-    string_array = ['a', 'b']
+    string_array = ['a']
     assert_equal ['a'], string_array
 
-    string_array = 'b'
+    string_array << 'b'
     assert_equal ['a', 'b'], string_array
 
     string_array.push 'C'
-    assert_equal ['a', 'b', 'c'], string_array
+    assert_equal ['a', 'b', 'C'], string_array
   end
 
-  def test_se_element_existe_no_array
+  def test_if_element_exist_in_array
     skip
 
     string_array = ['a', 'b', 'c']
 
-    assert_equal true, string_array.include?('d')
+    assert_equal false, string_array.include?('d')
     assert_equal true, !string_array.include?('z')
-    assert_equal false, !string_array.include?('A')
+    assert_equal true, !string_array.include?('A')
   end
 end
