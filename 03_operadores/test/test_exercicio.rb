e916bd2a4c990479a5459a1375c71ea9dd76ca21
Jardiel clevir <jardiel_maceio@hotmail.com>
2022-08-16 20:16:47 -0300

    Exercicio 04 finalizado

--- 03_operadores/test/test_exercicio.rb
+++ 03_operadores/test/test_exercicio.rb
@@ -6,44 +6,60 @@ class TestExercicio < Minitest::Test
   # 2. Não pode alterar nos valores das variáveis definidas
 
   def test_exercicio01
+    skip
+    
     i1, i2 = 1, 2
 
-    assert_equal 4, i1 + i2
-    assert_equal 0.5, i2 - i1
-    assert_equal -2, i2 * i1
-    assert_equal 5, i1 / i2
+    assert_equal 4, i1 + i2 + i1
+    assert_equal 0.5, i2 - i1 - 0.5
+    assert_equal -2, i2 * i1 - 4
+    assert_equal 5, i1 / i2 + 5 
     assert_equal 0.5, i1 / i2.to_f
   end
 
   def test_exercicio02
-    assert_equal 0, 10 % 3
+    skip
+
+    assert_equal 0, 10 % 3 -1
   end
+
 
   def test_exercicio03
+    skip
+    
     i1, i2 = 3, 5
 
-    assert_equal true, i1 == 2
-    assert_equal true, i2 == i1
-    assert_equal true, i1 > 3
+    assert_equal true, i1 == 2 + 1
+    assert_equal true, i2 == i1 + 2
+    assert_equal true, i1 > 3 -2
     assert_equal false, i2 < i1
-    assert_equal false, i1 <= 4
-    assert_equal false, i2 >= 4
+    assert_equal false, i1 <= 4 -2
+    assert_equal false, i2 >= 4 + 2
   end
+
 
   def test_exercicio04
+    skip
+  
     i1, i2 = 3, 5
 
-    assert_equal true, (i1 < 2) && (i2 > 6)
-    assert_equal true, (i1 > 4) || (i1 > i2)
-    assert_equal true, i1 == i2
+    assert_equal true, (i1 < 2) && (i2 > 6) || (i2 < 6)
+    assert_equal true, (i1 > 4) || (i1 > i2) || (i2 > i1)
+    assert_equal true, i1 == i2 || (i1 < i2)
   end
+  
 
   def test_exercicio05
+    
     i = 0
+    nulo = nil
 
-    assert_equal 2, i += 1
-    assert_equal -2, i -= 1
-    assert_equal 1, i ||= 2
-    assert_equal 2, i ||= 2
+    assert_equal 2, i += 1 + 1
+    assert_equal -2, i -= 1 + 3
+    assert_equal 1, i/i ||= 1 
+    assert_equal 2, i - i - i ||= 2 + 2
   end
+  
+
 end
+
