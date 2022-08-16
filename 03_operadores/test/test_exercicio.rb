5c00f693ead03fe661a84a27de13d26e76069799
Silvio Romero <silvioromeroneto@gmail.com>
2022-08-16 19:35:02 -0300

    Exercice 04 compete

--- 03_operadores/test/test_exercicio.rb
+++ 03_operadores/test/test_exercicio.rb
@@ -2,48 +2,55 @@ require "minitest/autorun"
 
 class TestExercicio < Minitest::Test
   # Regras
-  # 1. Não pode alterar os valores das expectativas (primeiro valor do assert_equal)
-  # 2. Não pode alterar nos valores das variáveis definidas
+  # 1. You are not allowed to change the expectations (the first value of assert_equal)
+  # 2. You are not allowed to change the value of the defined variables
 
-  def test_exercicio01
+  def test_exercice01
+
     i1, i2 = 1, 2
 
-    assert_equal 4, i1 + i2
-    assert_equal 0.5, i2 - i1
-    assert_equal -2, i2 * i1
-    assert_equal 5, i1 / i2
+    assert_equal 4, i1 + i2 + i1
+    assert_equal 0.5, i2 - i1 - i1.to_f
+    assert_equal -2, i2 * (-i1)
+    assert_equal 5, (i1 + 9) / i2
     assert_equal 0.5, i1 / i2.to_f
   end
 
-  def test_exercicio02
-    assert_equal 0, 10 % 3
+  def test_exercice02
+
+    assert_equal 0, 10 % 2
   end
 
-  def test_exercicio03
+  def test_exercice03
+
     i1, i2 = 3, 5
 
-    assert_equal true, i1 == 2
-    assert_equal true, i2 == i1
-    assert_equal true, i1 > 3
+    assert_equal true, i1 == 3
+    assert_equal true, !(i2 == i1)
+    assert_equal true, i1 >= 3
     assert_equal false, i2 < i1
-    assert_equal false, i1 <= 4
-    assert_equal false, i2 >= 4
+    assert_equal false, i1 >= 4
+    assert_equal false, i2 <= 4
   end
 
-  def test_exercicio04
+  def test_exercice04
+
     i1, i2 = 3, 5
 
-    assert_equal true, (i1 < 2) && (i2 > 6)
-    assert_equal true, (i1 > 4) || (i1 > i2)
-    assert_equal true, i1 == i2
+    assert_equal true, (i1 > 2) && (i2 < 6)
+    assert_equal true, (i1 < 4) || (i1 < i2)
+    assert_equal true, i1 != i2
   end
 
-  def test_exercicio05
+  def test_exercice05
+    
     i = 0
 
-    assert_equal 2, i += 1
-    assert_equal -2, i -= 1
+    assert_equal 2, i += 2
+    assert_equal -2, i *= -1
+    i += 3
     assert_equal 1, i ||= 2
+    i = nil
     assert_equal 2, i ||= 2
   end
 end
