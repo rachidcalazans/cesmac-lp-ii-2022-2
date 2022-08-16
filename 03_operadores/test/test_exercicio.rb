ed9622d81ea3783709e122cd88e95121df640a24
Cesmac <cesmac@academico.cesmac.edu.br>
2022-08-16 20:09:28 -0300

    exercicio03

--- 03_operadores/test/test_exercicio.rb
+++ 03_operadores/test/test_exercicio.rb
@@ -8,42 +8,42 @@ class TestExercicio < Minitest::Test
   def test_exercicio01
     i1, i2 = 1, 2
 
-    assert_equal 4, i1 + i2
-    assert_equal 0.5, i2 - i1
-    assert_equal -2, i2 * i1
-    assert_equal 5, i1 / i2
+    assert_equal 4, i1 + i1 + i2
+    assert_equal 0.5, i1 / i2.to_f
+    assert_equal -2, i1 - i2 -i1
+    assert_equal 5, i2 * i2 + i1
     assert_equal 0.5, i1 / i2.to_f
   end
 
   def test_exercicio02
-    assert_equal 0, 10 % 3
+    assert_equal 0, 10 % 10
   end
 
   def test_exercicio03
     i1, i2 = 3, 5
 
-    assert_equal true, i1 == 2
-    assert_equal true, i2 == i1
-    assert_equal true, i1 > 3
+    assert_equal true, i2 - i1 == 2
+    assert_equal true, i2 - 2 == i1
+    assert_equal true, i1 == 3
     assert_equal false, i2 < i1
-    assert_equal false, i1 <= 4
-    assert_equal false, i2 >= 4
+    assert_equal false, i1 > 4
+    assert_equal false, i2 < 4
   end
 
   def test_exercicio04
     i1, i2 = 3, 5
 
-    assert_equal true, (i1 < 2) && (i2 > 6)
-    assert_equal true, (i1 > 4) || (i1 > i2)
-    assert_equal true, i1 == i2
+    assert_equal true, (i1 > 2) && (i2 < 6)
+    assert_equal true, (i1 < 4) || (i1 > i2)
+    assert_equal true, i1 < i2
   end
 
   def test_exercicio05
     i = 0
 
-    assert_equal 2, i += 1
-    assert_equal -2, i -= 1
-    assert_equal 1, i ||= 2
-    assert_equal 2, i ||= 2
+    assert_equal 2, i +=2
+    assert_equal -2, i -=4
+    assert_equal 1, i +=3 
+    assert_equal 2, i +=1
   end
 end
