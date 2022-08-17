eb8fcc9e3550a72baf5a1b20c840d49bc639876a
Daniel Correia <danielgalego.dev@gmail.com>
2022-08-17 15:04:53 -0300

    calculator classes

--- 03_operadores/test/app/calculator.rb
+++ 03_operadores/test/app/calculator.rb
@@ -1,5 +1,64 @@
-class Calculus
-    def calculator(value_a, value_b, :operator)
-        result = value_a :operator value_b
-        return result
-    end  
+class Calculator
+
+    attr_reader :value
+
+    def initialize(*args)
+        @value = args
+        @result = calc
+    end
+
+    def result
+        @result
+    end
+end
+
+class Sum < Calculator
+    
+    def calc
+        @value.inject(:+)
+    end
+end
+
+class Sub < Calculator 
+
+    def calc
+        @value.inject(:-)
+    end
+end
+
+class Mult < Calculator
+
+    def calc
+        @value.inject(:*)
+    end
+end
+
+class Div < Calculator
+
+    def calc
+        @value.inject(:/)
+    end
+end
+
+class Pow < Calculator
+    
+    def calc
+        @value.inject(:**)
+    end
+end
+
+class Rem < Calculator
+
+    def calc
+        @value.inject(:%)
+    end
+end
+
+class Compairson < Calculator
+
+    def calc
+        @value.inject(:>=)
+    end 
+end
+
+
--- 03_operadores/test/test_desafio.rb
+++ 03_operadores/test/test_desafio.rb
@@ -1,5 +1,5 @@
 require "minitest/autorun"
-require "app/calculator"
+require_relative "app/calculator"
 
 # Challenge
 # It's worth 1.5 points if the PR is created until 17/08/2022 23:59:59
@@ -7,31 +7,36 @@ require "app/calculator"
 # Implement the method #calculator
 # Add an assert_equal to each test method with his respective operator
 
-class TestDesafio < Minitest::Test
-
-  def calculadora(valor_a, valor_b, operador)
-    return nil
-  end
+class TestChallenge < Minitest::Test
 
   def test_sum
-    assert_equal 2, calculadora(1, 1, :+)
+    assert_equal 3, Sum.new(1, 1, 1).result
   end
 
   def test_subtraction
+    assert_equal 5, Sub.new(10,5).result
   end
 
   def test_multiplication
+    assert_equal 20, Mult.new(1,10,2).result
   end
 
   def test_power
+    assert_equal 64, Pow.new(8,2).result
   end
 
   def test_division
+    assert_equal 5, Div.new(25,5).result
   end
 
   def test_remainder
+    assert_equal 0, Rem.new(20,4).result
   end
 
   def test_comparsion
+    assert_equal true, Compairson.new(120, 30).result
+  end
+
+  def test_boolean
   end
 end
