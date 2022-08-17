be002e0a1ff82c4df5ce8cc9b8d834983dc581dc
Aldo Nunes <aldonunes001@gmail.com>
2022-08-17 18:00:06 -0300

    Final version

--- 03_operadores/test/test_desafio.rb
+++ 03_operadores/test/test_desafio.rb
@@ -3,34 +3,59 @@ require "minitest/autorun"
 # Desafio
 # Vale 1.5 pontos válido se criar PR até 17/08/2022 23:59:59
 #
-# Implementar o método #calculadora
+# Implementar o método #calculator
 # Adicionar assert_equal para cada método de teste com seu respectivo operador
 
-class TestDesafio < Minitest::Test
+class TestChallenge < Minitest::Test
 
-  def calculadora(valor_a, valor_b, operador)
-    return nil
+  def calculator(value_a, value_b, operator)
+    if value_a.kind_of?(Numeric) && value_b.kind_of?(Numeric)
+      if operator == "+"
+        return value_a + value_b
+      elsif operator == "-"
+        return value_a - value_b
+      elsif operator == "*"
+        return value_a * value_b
+      elsif operator == "**"
+        return value_a ** value_b
+      elsif operator == "/" && value_b != 0
+        return value_a / value_b.to_f
+      elsif operator == "%"
+        return value_a % value_b
+      elsif operator == "="
+        return value_a == value_b
+      end
+    end
+
+    "Invalid Operation"
   end
 
-  def test_soma
-    assert_equal 2, calculadora(1, 1, '+')
+  def test_sum
+    assert_equal 2, calculator(1, 1, '+')
   end
 
-  def test_subtracao
+  def test_subtraction
+    assert_equal 5, calculator(8, 3, '-')
   end
 
-  def test_multiplicacao
+  def test_multiplication
+    assert_equal 12, calculator(6, 2, '*')
   end
 
-  def test_potencia
+  def test_power
+    assert_equal 8, calculator(2, 3, '**')
   end
 
-  def test_divisao
+  def test_division
+    assert_equal 2.5, calculator(5, 2, '/')
+    assert_equal "Invalid Operation", calculator(20, 0, '/')
   end
 
   def test_modulo
+    assert_equal 1, calculator(9, 2, '%')
   end
 
-  def test_comparacao
+  def test_comparison
+    assert_equal true, calculator(1, 1, '=')
   end
 end
