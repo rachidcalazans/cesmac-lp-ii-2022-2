b981a92f8e4763e0df5271a9fbe5a8dfe1499f74
Danilo Toledo <danilotoledo3rc@hotmail.com>
2022-08-18 08:43:13 -0300

    concluido

--- 03_operadores/test/test_desafio.rb
+++ 03_operadores/test/test_desafio.rb
@@ -9,28 +9,60 @@ require "minitest/autorun"
 class TestDesafio < Minitest::Test
 
   def calculadora(valor_a, valor_b, operador)
-    return nil
+    
+      if operador == '+'
+        return valor_a.to_f + valor_b
+
+      elsif operador == '-'
+        return valor_a - valor_b
+
+      elsif operador == '*'
+        return valor_a  * valor_b
+
+      elsif operador == "**"
+        return valor_a ** valor_b
+
+      elsif operador == '/'
+        return valor_a.to_f / valor_b
+
+      elsif operador == '%'
+        return valor_a % valor_b
+
+      elsif operador == "=="
+        return valor_a = valor_b
+      end
+
+      return nil
   end
 
   def test_soma
-    assert_equal 2, calculadora(1, 1, '+')
+    assert_equal 5, calculadora(3, 2, '+')
+    assert_equal 78.5, calculadora(70, 8.5, '+')
   end
 
   def test_subtracao
+    assert_equal 1, calculadora(2, 1, '-')
   end
 
   def test_multiplicacao
+    assert_equal 9, calculadora(3, 3, '*')
   end
 
   def test_potencia
+    assert_equal 9, calculadora(3, 2, "**")
   end
 
   def test_divisao
+    assert_equal 3, calculadora(6, 2, '/')
+    assert_equal 2.5, calculadora(5, 2, '/')
   end
 
   def test_modulo
+    assert_equal 0, calculadora(10, 2, '%')
   end
 
+  
   def test_comparacao
+    assert_equal 1, calculadora(1, 1, "==")
   end
 end
