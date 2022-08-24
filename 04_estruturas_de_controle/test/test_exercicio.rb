7dd255cb0884ee8cdfb1f9c5a537a3af17305621
Cesmac <cesmac@academico.cesmac.edu.br>
2022-08-24 20:39:43 -0300

    exercicio 04 feito

--- 04_estruturas_de_controle/test/test_exercicio.rb
+++ 04_estruturas_de_controle/test/test_exercicio.rb
@@ -4,4 +4,39 @@ class TestExercicio < Minitest::Test
   # Funcionalidade 1:
   # Retornar nome do mês do ano para qualquer número do mês passado.
   # Caso seja um número inválido, retornar message de erro
+def test_switch_case
+  mes = 12
+
+  case mes
+  when 1
+    mes_final = 'Janeiro'
+  when 2
+    mes_final = 'Fevereiro'
+  when 3
+    mes_final = 'Março'
+  when 4
+    mes_final = 'Abril'
+  when 5
+    mes_final = 'Maio'
+  when 6
+    mes_final = 'Junho'
+  when 7
+    mes_final = 'Julho'
+  when 8
+    mes_final = 'Agosto'
+  when 9
+    mes_final = 'Setembro'
+  when 10
+    mes_final = 'Outubro'
+  when 11
+    mes_final = 'Novembro'
+  when 12
+    mes_final = 'Dezembro'
+  else
+    mes_final = 'Mes inexistente'
+  end
+
+  assert_equal 'Dezembro', mes_final
+end
+
 end
