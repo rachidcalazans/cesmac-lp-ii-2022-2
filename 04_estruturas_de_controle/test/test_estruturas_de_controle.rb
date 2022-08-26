9693b7ece3ec559f7c6aac1907a857b5c21079d7
Roger Oliveira <82901003+New-Masster@users.noreply.github.com>
2022-08-25 22:22:24 -0300

    Realizado exercício 05

--- 04_estruturas_de_controle/test/test_estruturas_de_controle.rb
+++ 04_estruturas_de_controle/test/test_estruturas_de_controle.rb
@@ -46,7 +46,7 @@ class TestEstruturasDeControle < Minitest::Test
     end
 
     def test_for_each_loop
-      resultado_final = 0;
+      resultado_final = 0
 
       (1..5).each do |i|
         puts i
