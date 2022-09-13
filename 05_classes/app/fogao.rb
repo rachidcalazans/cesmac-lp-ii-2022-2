0a6010852ad7760bc6ea5fd6c3f520a42952e5e6
Mateus <mateuscpacifico04@gmail.com>
2022-09-13 19:59:09 -0300

    Exercice Done 05 (Stove one)

--- 05_classes/app/fogao.rb
+++ 05_classes/app/fogao.rb
@@ -1,7 +1,29 @@
-class Fogao
-  def initialize(marca, qtd_bocas)
-    @marca = marca
-    @qtd_bocas = qtd_bocas
-    @ligado = nil
+
+
+class Stove
+  def initialize(brand, qtd_burners)
+    @brand = brand
+    @qtd_burners = qtd_burners
+    @on = nil
+  end
+
+  def brand
+    @brand
+  end
+
+  def qtd_burners
+    @qtd_burners
+  end
+
+  def on?
+    @on
+  end
+
+  def on
+    @on = true
+  end
+
+  def off
+    @on = false
   end
 end
