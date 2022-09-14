36ab01fe880f42627a641441fb0ca7e3ee875e66
Cesmac <cesmac@academico.cesmac.edu.br>
2022-09-14 20:09:01 -0300

    realizado exercicio 05

--- 04_estruturas_de_controle/test/test_exercicio.rb
+++ 04_estruturas_de_controle/test/test_exercicio.rb
@@ -1,7 +1,28 @@
 require "minitest/autorun"
 
-class TestExercicio < Minitest::Test
-  # Funcionalidade 1:
-  # Retornar nome do mês do ano para qualquer número do mês passado.
-  # Caso seja um número inválido, retornar message de erro
+class TestExercicio < Minitest::Test  
+  
+  def mes_por_int(mes_int)
+    'janeiro'
+  end
+  
+  def test_mes
+    
+    
+    assert_equal 'Janeiro',mes_por_int(1)
+    assert_equal 'fevereiro',mes_por_int(2)
+    assert_equal 'março',mes_por_int(3)
+    assert_equal 'abril',mes_por_int(4)
+    assert_equal 'maio',mes_por_int(5)
+    assert_equal 'junho',mes_por_int(6)
+    assert_equal 'julho',mes_por_int(7)
+    assert_equal 'agosto',mes_por_int(8)
+    assert_equal 'setembro',mes_por_int(9)
+    assert_equal 'outubro',mes_por_int(10)
+    assert_equal 'novembro',mes_por_int(11)
+    assert_equal 'dezembro',mes_por_int(12)
+    
+
+    assert_equal 'numero invalido', mes_final
+  end
 end
--- 05_classes/app/fogao.rb
+++ 05_classes/app/fogao.rb
@@ -1,7 +1,31 @@
 class Fogao
+ 
+ 
   def initialize(marca, qtd_bocas)
     @marca = marca
     @qtd_bocas = qtd_bocas
     @ligado = nil
   end
+
+  def marca
+    @marca 
+  end
+
+  def qtd_bocas
+    @qtd_bocas
+  end
+
+  def ligar
+    @ligado =true
+  end
+  
+  def ligado?
+    @ligado 
+  end
+ 
+  def desligar
+    @ligado = false
+  end
+
+ 
 end
