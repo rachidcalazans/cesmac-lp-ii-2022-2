83271c985278dd85ca3e2b910c9b52087c3a8d54
Silvio Romero <silvioromeroneto@gmail.com>
2022-09-14 20:29:53 -0300

    Atividade 06 completa

--- 05_classes/app/fogao.rb
+++ 05_classes/app/fogao.rb
@@ -4,4 +4,27 @@ class Fogao
     @qtd_bocas = qtd_bocas
     @ligado = nil
   end
+
+  def marca
+    @marca
+  end
+
+  def qtd_bocas
+    @qtd_bocas
+  end
+
+  def ligado?
+    @ligado
+  end
+
+  def ligar
+    @ligado = true
+  end
+
+
+
+  def desligar
+    @ligado = false
+  end
+
 end
