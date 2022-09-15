c51147ef2620b6a2225bd7077452c74c0f80f832
Cesmac <cesmac@academico.cesmac.edu.br>
2022-09-14 21:35:23 -0300

    Desafio AF 2

--- 04_estruturas_de_controle/test/test_exercicio.rb
+++ 04_estruturas_de_controle/test/test_exercicio.rb
@@ -4,4 +4,33 @@ class TestExercicio < Minitest::Test
   # Funcionalidade 1:
   # Retornar nome do mês do ano para qualquer número do mês passado.
   # Caso seja um número inválido, retornar message de erro
+
+  def test_mes
 end
+
+def test_mes
+  case
+    when 1
+      mes_final = 'Janeiro'
+    when 2
+      mes_final = 'Fevereiro'
+    when 3
+      mes_final = 'Março'
+    when 4
+      mes_final = 'abril'
+    when 5
+      mes_final = 'maio'
+    when 6
+      mes_final = 'junho'
+    when 7
+      mes_final = 'julho'
+    when 8
+      mes_final = 'agosto'
+    when 9
+      mes_final = 'setembro'
+    when 10
+      mes_final = 'outubro'
+    when 11
+      mes_final = 'novembro'
+    when 12
+      mes_final = 'dezembro'
---
+++ 05_classes/test/test_pessoa_af2.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+
+require './05_classes/app/pessoa'
+
+class TestPessoa < Minitest::Test
+
+    def test_pessoa
+        pessoa = Pessoa.new('Thiago', 'Matheus')
+    
+        assert_equal 'Thiago', pessoa.nome()
+        assert_equal 'Matheus', pessoa.sobrenome()
+        assert_equal 'Thiago Matheus', pessoa.nome_completo()
+        
+    end
+end
+
+
