a93f31b9eda42628e8104e493e2672cdf2acaed7
Marechaal1 <alexandrex146@gmail.com>
2022-09-15 11:12:13 -0300

    Exercicio fogão concluido

--- 05_classes/app/fogao.rb
+++ 05_classes/app/fogao.rb
@@ -4,4 +4,24 @@ class Fogao
     @qtd_bocas = qtd_bocas
     @ligado = nil
   end
+
+  def marca
+    @marca
+  end
+
+  def qtd_bocas
+    @qtd_bocas
+  end
+
+  def ligar
+    @ligado = true
+  end
+
+  def ligado?
+    @ligado
+  end
+  
+  def desligar
+    @ligado = false
+  end
 end
