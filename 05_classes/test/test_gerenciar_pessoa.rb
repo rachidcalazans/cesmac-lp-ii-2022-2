18050d21746a5c1e6ff37f69cffcabea8f9b8def
Cesmac <cesmac@academico.cesmac.edu.br>
2022-09-21 21:11:02 -0300

    gustavo: criei teste de pessoa

--- 05_classes/test/test_gerenciar_pessoa.rb
+++ 05_classes/test/test_gerenciar_pessoa.rb
@@ -1,9 +1,16 @@
 require "minitest/autorun"
 require "./05_classes/app/gerenciar_pessoa"
+require "./05_classes/app/pessoa"
 
 class TestGerenciarPessoa < Minitest::Test
     def test_pessoas
-        gerenciar_pessoa = GerenciarPessoa.new
+        lista = []
+        gerenciar_pessoa = GerenciarPessoa.new(lista)
+
         assert_equal [], gerenciar_pessoa.pessoas
+
+        p1 = Pessoa.new('Abraao', 'Inexistente')
+        gerenciar_pessoa = GerenciarPessoa.new([p1])
+        assert_equal [p1], gerenciar_pessoa.pessoas
     end
 end
