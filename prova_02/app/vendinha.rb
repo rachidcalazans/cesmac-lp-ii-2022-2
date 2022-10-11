22a536dc7b82c2441a6b0614ce1451793af79f68
Kkleberfilho <kleberfilho.dev@gmail.com>
2022-10-11 18:05:36 -0300

    Kleber: criei a classes Vendinha e Flau e as classes de test

---
+++ prova_02/app/vendinha.rb
@@ -0,0 +1,26 @@
+class Vendinha
+    def initialize 
+        @total = 0
+        @flaus = []
+    end 
+    
+    def realizar_venda
+        
+        
+    end    
+end 
+
+class Flau
+    def initialize nome , valor 
+        @nome = nome 
+        @valor = valor 
+    end
+    
+    def nome 
+        @nome
+    end 
+    
+    def valor 
+        @valor 
+    end     
+end 
---
+++ prova_02/test/test_vendinha.rb
@@ -0,0 +1,9 @@
+require "minitest/autorun"
+require './prova_2/app/vendinha'
+
+
+
+class TestVendinha < Minitest::Test
+
+
+end
