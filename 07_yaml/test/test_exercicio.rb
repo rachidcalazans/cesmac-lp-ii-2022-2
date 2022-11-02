96f5c8d5aaa24a60d342ff4655cc4f8a84aed801
Marechaal1 <alexandrex146@gmail.com>
2022-11-02 10:54:43 -0300

    Arquivos de alunos criado e alterado

--- 07_yaml/test/test_exercicio.rb
+++ 07_yaml/test/test_exercicio.rb
@@ -1,4 +1,5 @@
 require "minitest/autorun"
+require 'yaml'
 
 class TestExercicio < Minitest::Test
   # A lista de Hash abaixo deve ser salvo dentro de um arquivo YAML.
@@ -15,4 +16,44 @@ class TestExercicio < Minitest::Test
   # 1. Quando o nome for Joao, a matrícula deverá ser atualizada para '0004'
   # 2. Quando a matricula for '0001', o nome deverá ser atualizado para 'Ze Felipe'
   # 3. Quando a matricula for '0003', deve ser removido da lista
+
+  def test_criar_lista
+    students = [
+    {nome: 'Ze' , matricula:'0001'},
+    {nome:'Joao', matricula: '0002'},
+    {nome:'Maria', matricula: '0003'}
+    ]
+
+    File.open('07_yaml/test/students.yml', 'w') do |file|
+      file.write(students.to_yaml)
+    end
+    
+    students_file = File.read('07_yaml/test/students.yml')
+    load_students = YAML.load(students_file)
+
+    load_students.each do |student|
+      if student[:nome] == 'Joao'
+        student[:matricula] = '0004'
+      end
+
+      if student[:matricula] == '0001'
+        student[:nome] = 'Ze Felipe'
+    
+      elsif student[:matricula] == '0003'
+          students.delete(student)
+   
+      end
+    end
+
+    File.open('07_yaml/test/students.yml', 'w') do |file|
+      file.write(students.to_yaml)
+    end
+    
+    students_file = File.read('07_yaml/test/students.yml')
+    load_students = YAML.load(students_file)
+
+    assert_equal '0002', load_students[1][:matricula]
+
+    
+  end
 end
