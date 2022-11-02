25778939edf7bba571fe05b3c57c1120ff7d381c
Aldo Nunes <aldonunes001@gmail.com>
2022-11-02 17:18:47 -0300

    Challenge03 completed

--- 06_hash/test/test_hash_exercicio.rb
+++ 06_hash/test/test_hash_exercicio.rb
@@ -1,4 +1,64 @@
 require "minitest/autorun"
 
 class TestHashExercicio < Minitest::Test
+
+  def test_1
+    hash = [ { name: 'Ze', registration: '0001' }, 
+             { name: 'Joao', registration: '0002' }, 
+             { name: 'Maria', registration: '0003' } ]
+
+    hash.each do |i|
+
+      if i[:name] == 'Joao'
+        i[:registration] = '0004'
+      end
+    end
+
+
+    expected_hash = [ { name: 'Ze', registration: '0001' }, 
+                      { name: 'Joao', registration: '0004' }, 
+                      { name: 'Maria', registration: '0003' } ]
+
+    assert_equal expected_hash, hash
+  end
+
+  def test_2
+    hash = [ { name: 'Ze', registration: '0001' }, 
+             { name: 'Joao', registration: '0002' }, 
+             { name: 'Maria', registration: '0003' } ]
+
+    hash.each do |i|
+
+      if i[:registration] == '0001'
+        i[:name] = 'Ze Felipe'
+      end
+    end
+
+
+    expected_hash = [ { name: 'Ze Felipe', registration: '0001' }, 
+                      { name: 'Joao', registration: '0002' }, 
+                      { name: 'Maria', registration: '0003' } ]
+
+    assert_equal expected_hash, hash
+  end
+
+  def test_3
+    hash = [ { name: 'Ze', registration: '0001' }, 
+             { name: 'Joao', registration: '0002' }, 
+             { name: 'Maria', registration: '0003' } ]
+
+    hash.each do |i|
+
+      if i[:registration] == '0003'
+        hash.delete(i)
+      end
+    end
+
+
+    expected_hash = [ { name: 'Ze', registration: '0001' }, 
+                      { name: 'Joao', registration: '0002' } ]
+
+    assert_equal expected_hash, hash
+  end
+
 end
