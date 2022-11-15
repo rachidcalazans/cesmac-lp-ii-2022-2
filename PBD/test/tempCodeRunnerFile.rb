7ca365a884707aa31c94d23b6975be8013dee9d6
Aldo Nunes <aldonunes001@gmail.com>
2022-11-15 16:31:01 -0300

    1o exercise from PBD

---
+++ PBD/test/tempCodeRunnerFile.rb
@@ -0,0 +1,14 @@
+def test_create
+  #   db.execute("create table if not exists test(id integer, name string)")
+
+  #   names = ['Jay', 'Kay', 'Ray']
+  #   for i in 1..3
+  #     db.execute("insert into test values(?, ?)", i, names[i-1])
+  #   end
+
+  #   query = db.execute("select * from test")
+
+  #   assert_equal true, File.exist?('test.db')
+
+  #   #assert_equal 1, query[0][0]
+  # end
