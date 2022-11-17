653045bc7714f9798af8bc6d4553baa68e7ea63c
Aldo Nunes <aldonunes001@gmail.com>
2022-11-17 20:05:50 -0300

    version 3.0

--- prova_03/test/test_lectures.rb
+++ prova_03/test/test_lectures.rb
@@ -1,4 +1,5 @@
 require "minitest/autorun"
+require "date"
 
 class TestExercicio < Minitest::Test
 
@@ -6,11 +7,10 @@ class TestExercicio < Minitest::Test
 
     # DECLARE ALL VARIABLES
     #all_lectures = File.readlines('prova_03/proposals.txt').map(&:chomp)
-    all_lectures = []
-    duration_of_lectures = []
+    all_lectures = {}
 
-    schedule_morning = []  # final
-    schedule_afternoon = [] # final
+    schedule_morning = {}  # final
+    schedule_afternoon = {} # final
 
     track_time_morning = 60 * 3
     track_time_afternoon = (60 * 4) - 1
@@ -18,80 +18,56 @@ class TestExercicio < Minitest::Test
     #READ THE FILE
     File.foreach("prova_03/proposals.txt") do |line|
       if line != "" and line != "\n"
-        all_lectures.append(line[0..-2])
-        duration_of_lectures.append(line[-6..-5])
+        all_lectures[line[0..-2]] = line[-6..-5]
       end
     end
 
-    # print(all_lectures)
-    # print(all_lectures.length)
-    # # print(duration_of_lectures)
-    # print(duration_of_lectures.length)
-    
+    time = DateTime.new(2022, 11, 17, 9, 0)
     # FILL THE MORNING SCHEDULE
-    all_lectures.each_with_index do |item, i|
-      if duration_of_lectures[i] == "tn"
-        time = 5
+    all_lectures.each do |key, value|
+      if value == 'tn'
+        duration = 5
       else
-        time = duration_of_lectures[i].to_i
+        duration = value.to_i
       end
 
-      #print("time=#{time} track_time=#{track_time_morning} \n")
-      if time <= track_time_morning
-        schedule_morning.push(item)
-        all_lectures.delete_at(i)
-        duration_of_lectures.delete_at(i)
-
-        track_time_morning -= time  
-
-        if track_time_morning <= 0
-          break
-        end
-      end
-    end
-    
-    all_lectures.compact!
-    duration_of_lectures.compact!
-
-    #print("\n")
-    #print(all_lectures)
-    # # print(all_lectures.length)
-    #print(duration_of_lectures)
-    # print(duration_of_lectures.length)
-    #print(schedule_morning)
-
-    #FILL THE AFTERNOON SCHEDULE
-    all_lectures.each_with_index do |item, i|
-      if duration_of_lectures[i] == "tn"
-        time = 5
-      else
-        time = duration_of_lectures[i].to_i
+      if duration <= track_time_morning
+        schedule_morning[key] = time.strftime("%H:%M")
+        all_lectures.delete(key)
+        track_time_morning -= duration
+        time += (1.0/24/60) * duration
       end
 
-      # print("#{all_lectures[i]} time=#{time} track_time=#{track_time_afternoon} \n")
-      if time <= track_time_afternoon
-        schedule_afternoon.push(item)
-        all_lectures.delete_at(i)
-        duration_of_lectures.delete_at(i)
-        track_time_afternoon -= time  
-
-        if track_time_afternoon <= 0
-          break
-        end
+      if track_time_morning <= 0
+        break
       end
     end
 
-    print("\n")
-    #print(all_lectures)
-    # # print(all_lectures.length)
-    #print(duration_of_lectures)
-    # print(duration_of_lectures.length)
-    #print(schedule_afternoon)
+    time = DateTime.new(2022, 11, 17, 13, 0)
+    # FILL THE AFTERNOON SCHEDULE 
+    all_lectures.each do |key, value|
+      if value == 'tn'
+        duration = 5
+      else
+        duration = value.to_i
+      end
+
+      if duration <= track_time_afternoon
+        schedule_afternoon[key] = time.strftime("%H:%M")
+        all_lectures.delete(key)
+        track_time_afternoon -= duration
+        time += (1.0/24/60) * duration
+      end
+
+      if track_time_afternoon <= 0
+        break
+      end
+    end
 
     puts("Track A:")
 
-    schedule_morning.each do |item|
-      puts item
+    schedule_morning.each do |key, value|
+      puts("\n" + value + " " + key)
     end
 
 
