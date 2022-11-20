859474ade30442c7c0f65349fcb9f6e0f59c3512
ViniCrK <vinicrk@outlook.com>
2022-11-20 01:16:22 -0300

    Método para leitura do arquivo proposals.txt e conversação para uma lista

--- prova_03/test/test_prova.rb
+++ prova_03/test/test_prova.rb
@@ -1,8 +1,14 @@
 require 'minitest/autorun'
-require 'sqlite3'
 
 class TestProva < Minitest::Test
-    def test_db
-        db = SQLite3::Database.open 'palestras.db'
+    def test_leitura
+        arq = File.new("./prova_03/proposals.txt", "r")
+        assert_equal true, File.exist?("./prova_03/proposals.txt")
+
+        lista = File.readlines("./prova_03/proposals.txt")
+        assert_equal "Apresentando Lua para as massas 30min\n", lista[2]
+
+        puts lista
     end
 end
+
