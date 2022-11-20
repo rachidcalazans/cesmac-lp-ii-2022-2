511582c8cc10c53c44de7c834ee351db8224bcbb
Roger Oliveira <82901003+New-Masster@users.noreply.github.com>
2022-11-19 21:37:08 -0300

    Criado o arquivo date para trabalhar na class

--- prova_03/ap_03.rb
+++ prova_03/ap_03.rb
@@ -1,12 +1,21 @@
 require 'minitest/autorun'
-require 'yaml'
+require './prova_03/date'
 
 class Proposals < Minitest::Test
-    def test_criar_arquivo_yaml
-        tweets = IO.readlines("prova_03/proposals.txt").each_with_index.map do |line, line_num|
-            [line_num, line]
-          end
-          puts tweets[0]
+    def test_open_file
+        speeches = [] #palestras
+        duraction = [] #duração
+        
+
+        morning = [] #Manhã
+        afternoon = [] #Tarde
+
+        speeches = File.readlines("prova_03/proposals.txt").each_with_index.map do |line|
+            line [line]
+        end
+        
+        puts speeches
     end
-    
-end
+
+
+end    
