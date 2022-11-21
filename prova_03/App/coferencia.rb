d92c04f6aa88b1c11dc2ec9bec05751ad623d55d
kkleberfilho <kleberfilho.dev@gmail.com>
2022-11-21 19:02:38 -0300

    Criei os metodos initialize, organizar plestars, organizar tempo palestras

--- prova_03/App/coferencia.rb
+++ prova_03/App/coferencia.rb
@@ -2,10 +2,35 @@ require './prova_03/proposals'
 require 'time'
 
 class Workshop
+    def initialize(lista_palestra)
+        @lista_palestra = lista_palestra
+        @palestras = File.readlines(@lista_paletra, chomp: true)
+        @tempopalestra 
+        @horario = Time.new(2022,12,15,9)
+    end
 
-    
+     
+    def organizar_tempoPalestras
+        @tempopalestra = @palestras.map do |minutes|
+            if (minutes.match(/lightning/))
+                minutes = '5'
+            end
+            minutes.match(/\d+/)[0].to_i
+        end 
+        @tempopalestra
+    end
 
+    def organizarPalestras
+        @palestras
+    end   
 
+    def organizar_tempoPalestras
+        organizar_tempoPalestras = []
+        organizar_tempoPalestras.push(@horario.strftime("%k:%m".strip))
+
+        return organizar_tempoPalestras
+
+    def 
 
 
 end 
