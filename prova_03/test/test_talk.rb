13733acf4630893fc3adfc117f8ea66819021a27
Yago <yagolins07@gmail.com>
2022-11-22 18:57:50 -0300

    prova03

---
+++ prova_03/test/test_talk.rb
@@ -0,0 +1,59 @@
+require "minitest/autorun"
+require './prova_03/app/talk'
+
+
+class TestTalks < Minitest::Test
+
+    def test_talks
+
+        talk = Talk.new ("./prova_03/proposals.txt")
+
+        expected_listas_talk = [
+
+            'Diminuindo tempo de execução de testes em aplicações Rails enterprise 60min',
+            'Reinventando a roda em ASP clássico 45min',
+            'Apresentando Lua para as massas 30min',
+            'Erros de Ruby oriundos de versões erradas de gems 45min',
+            'Erros comuns em Ruby 45min',
+            'Rails para usuários de Django lightning',
+            'Trabalho remoto: prós e cons 60min',
+            'Desenvolvimento orientado a gambiarras 45min',
+            'Aplicações isomórficas: o futuro ( que talvez nunca chegaremos ) 30min',
+            'Codifique menos, Escreva mais! 30min',
+            'Programação em par 45min',
+            'A mágica do Rails: como ser mais produtivo 60min',
+            'Ruby on Rails: Por que devemos deixá-lo para trás 60min',
+            'Clojure engoliu Scala: migrando minha aplicação 45min',
+            'Ensinando programação nas grotas de Maceió 30min',
+            'Ruby vs. Clojure para desenvolvimento backend 30min',
+            'Manutenção de aplicações legadas em Ruby on Rails 60min',
+            'Um mundo sem StackOverflow 30min',
+            'Otimizando CSS em aplicações Rails 30min'
+        ]
+
+        assert_equal expected_listas_talk, talk.show_talk
+
+    end
+
+    def test_talk_time
+
+        talk = Talk.new ('./prova_03/proposals.txt')
+
+        expected_min = [60, 45, 30, 45, 45, 5, 60, 45, 30, 30, 45, 60, 60, 45, 30, 30, 60, 30, 30]
+
+        assert_equal talk.show_time_talks
+, expected_min
+
+    end
+
+    def test_hours
+
+        talk = Talk.new ('./prova_03/proposals.txt')
+
+        expected_hours = ["9:00"]
+
+        assert_equal expected_hours, talk.show_time
+
+    end
+
+end
