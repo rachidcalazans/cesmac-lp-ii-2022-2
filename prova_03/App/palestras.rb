2ac62833ffdd4fde508fa09e14deb36bc917debf
kkleberfilho <kleberfilho.dev@gmail.com>
2022-11-23 20:42:11 -0300

    Implementei a classe palestra

--- prova_03/App/palestras.rb
+++ prova_03/App/palestras.rb
@@ -1,7 +1,11 @@
 class Palestras 
     attr_accessor :nome, :duraca, :horario 
 
-    def initialize
+    def initialize nome, duracao, horario, conferencia
+        @nome = nome
+        @duracao = duracao 
+        @horario = horario 
+        
 
     end
 
