4fa4e0f9f0291ea58ac6fb5486dfe38f78f80605
lipevieiraa <felipe.vieirabarross@gmail.com>
2022-11-23 09:36:06 -0300

    prova 03

--- prova_03/app/conferencia.rb
+++ prova_03/app/conferencia.rb
@@ -1,4 +1,29 @@
+require 'time'
 
     class Conferencia
+        def initialize 
+            @palestra
+            @tempo_falas
+            @tempo 
+        end
+            
+            def palestra_lista
+                @palestra
+            end 
+            
+            def tempo_falas
+                @tempo_falas
+                @talkt = @lecture.map do |minutos|
+                    if (minutos.match(/lightning/))
+                        minutos = '5'
+                    end
+            minutos.match(/\d+/)[0].to_i
+        end
+            def lista_hora
+                    lista_hora = []
+                    lista_hora.push(@hora.strftime("%k:%M").strip)
+    
+            return lista_hora
+        end
     end
         
