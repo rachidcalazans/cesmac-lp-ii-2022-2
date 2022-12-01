2d53d459dbbafaf0001f9156c904c2ef19c1f5a0
Cesmac <cesmac@academico.cesmac.edu.br>
2022-11-30 21:13:46 -0300

    criação de banco de dados

--- Dojo/cadastro/cadastro.rb
+++ Dojo/cadastro/cadastro.rb
@@ -6,22 +6,25 @@ class CadastroDb < Minitest::Test
   def test_cadastro_db
     db = SQLite3::Database.open 'cadastro.db'
     db.results_as_hash = true
-    db.execute 'CREATE TABLE IF NOT EXISTS alunos(nome TEXT, matricula TEXT, participacao INT)'
+    db.execute 'CREATE TABLE IF NOT EXISTS alunos(nome TEXT, matricula INT)'
     db.execute 'DELETE FROM alunos'
-    db.execute 'INSERT INTO alunos (nome, participacao, matricula) VALUES (?, ?, ?)', 'Ze', 0, '001'
-    db.execute 'INSERT INTO alunos (nome, participacao, matricula) VALUES (?, ?, ?)', 'Joao', 0, '002'
-    results = db.execute 'SELECT nome, matricula, participacao FROM alunos WHERE matricula=?', '002'
+    db.execute 'INSERT INTO alunos (nome, matricula) VALUES (?, ?)', 'Maria Antonieta', 00012345
+    db.execute 'INSERT INTO alunos (nome, matricula) VALUES (?, ?)', 'João Carlos', 00054321
+    results = db.execute 'SELECT nome, matricula  FROM alunos'
     # results = db.execute 'SELECT * FROM alunos'
     results_array = results
 
     expected_result = [
       {
-        'nome' => 'Joao',
-        'participacao' => 0,
-        'matricula' => '002'
+        'nome' => 'Maria Antonieta',
+        'matricula' => 00012345
       },
+      {
+        'nome' => 'João Carlos',
+        'matricula' => 00054321
+      }
     ]
-    assert_equal 1, results_array.size
+    assert_equal 2, results_array.size
     assert_equal results_array, expected_result
   end
 end
