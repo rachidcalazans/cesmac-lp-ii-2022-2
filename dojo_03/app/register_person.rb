dc84a0ad700130c924726425ad16161ba22a848a
Daniel Correia <danielgalego.dev@gmail.com>
2022-11-30 21:19:49 -0300

    first commit

---
+++ dojo_03/app/register_person.rb
@@ -0,0 +1,5 @@
+class Register
+  def initialize
+    
+  end
+end
---
+++ dojo_03/test/test_register_person.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+
+class TestRegister < Minitest::Test 
+  def test_registor_person
+    assert_equal 1, 1
+  end
+end
